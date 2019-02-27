264997a285154f46809cf829a4942e42f420236c
Cara Ponzini <ponzini@gmail.com>
2019-02-26 22:03:45 -0800

    one passing test for keyword filter

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import images from '../data/images.js';
+import makeImageTemplate from './make-image-template.js';
 
 const imageDisplay = document.getElementById('image-display');
-
-//create template
-
-function makeImageTemplate(image) {
-
-    const html = 
-        `<li class="${image.keyword}">
-            <h2>${image.title}</h2>
-            <img src="${image.url}" alt="${image.description}">
-            <p>Horns: ${image.horns}</p>
-        </li>`;
-
-
-    const template = document.createElement('template');
-    template.innerHTML = html;
-
-    return template.content;
-    
-}
 
 //make dom
 
--- test/tests.js
+++ test/tests.js
@@ -1,2 +1,33 @@
-
 import './html-equal.js';
+
+const test = QUnit.test;
+
+//make array of images
+
+const images = [
+    { title: 'unicorn', keyword: 'unicorn', horns: 1 },
+    { title: 'double unicorn', keyword: 'unicorn', horns: 2 },
+    { title: 'triceratops', keyword: 'dinosaur', horns: 3 }
+];
+
+function filterImages(images, filter) {
+    return images.filter(image => {
+        const hasKeyword = image.keyword === filter.keyword;
+
+        return hasKeyword;
+    });
+}
+
+test('filter only displays images with matching keyword of unicorn', assert => {
+//arrange
+
+    const filter = { keyword: 'unicorn' };
+//act
+    let filtered = filterImages(images, filter);
+
+//assert
+    assert.deepEqual(filtered, [
+        { title: 'unicorn', keyword: 'unicorn', horns: 1 },
+        { title: 'double unicorn', keyword: 'unicorn', horns: 2 }
+    ]);
+});
