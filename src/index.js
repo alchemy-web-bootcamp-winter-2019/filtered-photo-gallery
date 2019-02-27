003b4a0117732a206d3272fcf410eb6c953a2045
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-26 16:55:36 -0800

    added form to html

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import { makeImageTemplate } from './make-list-template.js';
 import images from '../data/images.js';
 import loadImages from './make-list-template.js';
 const imageList = document.getElementById('image-list');
+
+loadImages(images);
