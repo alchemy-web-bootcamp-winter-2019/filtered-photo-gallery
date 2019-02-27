3ab3d390d83ac41f7916c57a37498a932665f334
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-02-26 18:11:10 -0800

    started first test, no internet though so can't test just yet. bus code!

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,32 @@
 import './html-equal.js';
 
 const test = QUnit.test;
+function createImageLI(imageObj) {
+  const template = document.createElement('template');
+  const html =/*html*/ `<li>
+  <h2>Title</h2>
+  <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+<li>`
+  template.innerHTML = 
+}
 
+const imageObj = {
+  url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+  title: 'UniWhal',
+  description: 'A unicorn and a narwhal nuzzling their horns',
+  keyword: 'narwhal',
+  horns: 1
+};
+
+test('test function which creates interpolated template', assert => {
+  //arrange
+  const expected = /*html*/ `<li>
+    <h2>Title</h2>
+    <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+  <li>`;
+
+  //act
+  const result = createImageLI(imageObj);
+  //assert
+  assert.htmlEqual(result, expected);
+});
