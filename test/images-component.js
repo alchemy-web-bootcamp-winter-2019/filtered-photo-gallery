565513188b6adc60f3b9b349be2b646f9ba1ca57
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-02-27 08:46:54 -0800

    testing

--- test/images-component.js
+++ test/images-component.js
@@ -3,4 +3,42 @@ import makeImageTemplate from '../src/image-template.js';
 
 const test = QUnit.test;
 
-test('')
+
+function makeImageFilter(images){
+    images.forEach(image => {
+        const dom = makeImageTemplate(image);
+        imageList.appendChild(dom);
+    })
+}
+
+test('image filter test', function(assert) {
+    //arrange
+    const images = [
+        {
+            keyword: 'rhino',
+            horns: 2
+        },
+        {
+            keyword: 'rhino',
+            horns: 1
+        },
+        {
+            keyword: 'chameleon',
+            horns: 5
+        },
+    ];
+    //act
+    const result = makeImageFilter(images);
+    //assert
+    assert.deepEqual(result, [
+        {
+            keyword: 'rhino',
+            horns: 2
+        },
+        {
+            keyword: 'rhino',
+            horns: 1
+        }
+    ]);
+
+});
