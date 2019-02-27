b6686e03c83d591a1f36cc36791af86cc0e1bbea
Leslie Shea <leslieyshea@gmail.com>
2019-02-26 16:39:00 -0800

    moved function outside and imported

--- test/make-image-list.test.js
+++ test/make-image-list.test.js
@@ -1,19 +1,8 @@
 import images from '../data/images.js';
+import makeImageTemplate from '../src/make-image-list.js';
 
 const test = QUnit.test;
 
-
-function makeImageTemplate(image) {
-    const html = /*html*/
-    `<li>
-        <h2>${image.title}</h2>
-        <img src="${image.url}">
-    </li>
-    `;
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
 
 test('makes image list template', assert => {
     // arrange
