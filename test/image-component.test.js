7ec0d3be1bc9fefc1698a8615bcd0d2ace0c3478
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-26 16:47:22 -0800

    Bug for Marty
    See line 14 of html-equal.js It looks like outerHTML is converting & to
     &amp;

--- test/image-component.test.js
+++ test/image-component.test.js
@@ -3,13 +3,17 @@ const test = QUnit.test;
 QUnit.module('creating image list template');
 
 function createImageTemplate(image) {
-    return /*html*/`
+    const html = /*html*/`
         <li>
             <h2>${image.title}</h2>
-            <img src="${image.url}">
+            <img src=${image.url}>
             <h3>Horns: ${image.horns}</h3>
         </li>
     `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    const dom = template.content;
+    return dom;
 }
 
 test('create image template', assert => {
@@ -25,9 +29,9 @@ test('create image template', assert => {
         title: 'Rhino Family',
         url: 'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80',
         horns: 2
-    }
+    };
     // act
     const result = createImageTemplate(image);
     // assert
-    assert.equal(result, expected);
+    assert.htmlEqual(result, expected);
 });
