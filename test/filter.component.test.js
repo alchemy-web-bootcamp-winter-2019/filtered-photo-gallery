2038a58235e1ca414b5a8c06cfd94a1f84fa2ee9
Leslie Shea <leslieyshea@gmail.com>
2019-02-26 19:13:10 -0800

    created tests and wrote function to filter by different filters

--- test/filter.component.test.js
+++ test/filter.component.test.js
@@ -2,17 +2,19 @@ const test = QUnit.test;
 
 function filterImages(images, filter) {
     return images.filter(function(image) {
-        const hasKeyword = image.keyword === filter.keyword;
+        const hasKeyword = !filter.keyword || image.keyword === filter.keyword;
+
+        const hasHorns = !filter.horns || image.horns === filter.horns;
     
-        return hasKeyword;
+        return hasKeyword && hasHorns;
 
     });
 }
 
 const images = [
     { keyword: 'blue', horns: 1 },
-    { keyword: 'red', horns: 2 },
-    { keyword: 'blue', horns: 1 },
+    { keyword: 'blue', horns: 2 },
+    { keyword: 'red', horns: 1 },
     
 ];
 
@@ -24,6 +26,47 @@ test('image list filters by keyword only', assert => {
     const result = filterImages(images, filter);
     const expected = [
         { keyword: 'blue', horns: 1 },
+        { keyword: 'blue', horns: 2 }
+    ];
+
+    // assert
+    assert.deepEqual(result, expected);
+});
+
+test('no filters returns all images', assert => {
+    // arrange
+    const filter = { keyword: '' };
+
+    // act
+    const result = filterImages(images, filter);
+    const expected = images;
+
+    // assert
+    assert.deepEqual(result, expected);
+});
+
+test('image list filters by horns only', assert => {
+    // arrange
+    const filter = { horns: 1 };
+
+    // act
+    const result = filterImages(images, filter);
+    const expected = [
+        { keyword: 'blue', horns: 1 },
+        { keyword: 'red', horns: 1 }
+    ];
+
+    // assert
+    assert.deepEqual(result, expected);
+});
+
+test('image list filters by keyword and horns', assert => {
+    // arrange
+    const filter = { keyword: 'blue', horns: 1 };
+
+    // act
+    const result = filterImages(images, filter);
+    const expected = [
         { keyword: 'blue', horns: 1 }
     ];
 
