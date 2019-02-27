bcaad2ab25b2a79b35a3c07407deb576ce2eb7dd
mcnadams <mcneilbo@oregonstate.edu>
2019-02-26 16:49:20 -0800

    make loadFilter with callback (but haven't made filterImages yet)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import images from '../data/images.js';
 import { loadImages } from './make-html-template.js';
 
+const filterForm = document.getElementById('filter-form');
+
 loadImages(images);
+
+function loadFilter(callback) {
+    filterForm.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const formData = new FormData(filterForm);
+        const filter = {
+            keyword: formData.get('keyword-filter'),
+            horns: formData.get('horns-filter')
+        };
+        callback(filter);
+    });
+}
+
+loadFilter(filter => {
+    const filteredImages = filterImages(filter, images);
+    loadImages(filteredImages);
+});
