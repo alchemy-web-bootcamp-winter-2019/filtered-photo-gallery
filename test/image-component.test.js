d6e718118533e774576d48294ffc4f6889518e98
Cheri Portman <cheriportman@hotmail.com>
2019-02-26 16:57:55 -0800

    makeImageComponent( function works when grabbing image dynamically

--- test/image-component.test.js
+++ test/image-component.test.js
@@ -1,15 +1,15 @@
 import images from '../data/images.js';
-
+console.log(images[0]);
 const test = QUnit.test;
 
 QUnit.module('template creation test');
 
-function makeImageComponent() {
+function makeImageComponent(images) {
     const html = /*html*/`
         <li>
-            <h2>Name: UniWhal</h2>
-            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
-            <h2>Horns: 1</h2>
+            <h2>Name: ${images[0].title}</h2>
+            <img src="${images[0].url}">
+            <h2>Horns: ${images[0].horns}</h2>
         </li>
     `;
     const template = document.createElement('template');
@@ -30,7 +30,7 @@ test('create a template to create image tiles', function(assert) {
 
 
     //act
-    const result = makeImageComponent();
+    const result = makeImageComponent(images);
 
     //assert
     assert.htmlEqual(result, expected);
