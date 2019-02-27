b77711b6c590c1cb99181eea77add4f7afef3940
Payton Lower <paytown@me.com>
2019-02-26 18:49:18 -0800

    working through filter tests. taking break. continue tomorrow.

--- test/tests.js
+++ test/tests.js
@@ -4,8 +4,41 @@ import './html-equal.js';
 import './make-html.test.js';
 
 
+// const image = [
+//     {
+//         horns: 1,
+//         keyword: 'narwhal'
+//     },
+//     {
+//         horns: 2,
+//         keyword: 'narwhal'
+//     },
+//     {
+//         horns: 2,
+//         keyword: 'rhino'
+//     }
+    
+// ];
+
 // Testing for filters
 
 test('filter by keyword', assert => {
 
+    const result = [
+        {
+            horns: '',
+            keyword: 'narwhal'
+        },
+        {
+            horns: '',
+            keyword: 'narwhal'
+        }
+    ];
+
+    const expected = {
+        keyword: 'narwhal',
+        horns: ''
+    };
+
+    assert.htmlEqual(result, expected);
 });
