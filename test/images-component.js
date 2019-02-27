b88cffeb4cb528be6fc32a7f8b2c115a95ec8a9f
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-02-27 11:18:10 -0800

    pass first test building second test

--- test/images-component.js
+++ test/images-component.js
@@ -1,14 +1,13 @@
 import './html-equal.js';
-import makeImageTemplate from '../src/image-template.js';
-
+// import makeImageTemplate from '../src/image-template.js';
+// const imageList = document.getElementById('image-list');
 const test = QUnit.test;
 
-
-function makeImageFilter(images){
-    images.forEach(image => {
-        const dom = makeImageTemplate(image);
-        imageList.appendChild(dom);
-    })
+function makeImageFilter(images, filter){
+    return images.filter(image => {
+        const hasKeyword = image.keyword === filter.keyword;
+        return hasKeyword;
+    });
 }
 
 test('image filter test', function(assert) {
@@ -21,16 +20,20 @@ test('image filter test', function(assert) {
         {
             keyword: 'rhino',
             horns: 1
-        },
-        {
-            keyword: 'chameleon',
-            horns: 5
-        },
+        }
+
     ];
+
+    const filter = { keyword: 'rhino', horns: 2 };
     //act
-    const result = makeImageFilter(images);
+    const filtered = makeImageFilter(images, filter);
     //assert
-    assert.deepEqual(result, [
+    assert.deepEqual(filtered, images);
+});
+
+test('show all images', function(assert) {
+    //arrange
+    const images = [
         {
             keyword: 'rhino',
             horns: 2
@@ -39,6 +42,13 @@ test('image filter test', function(assert) {
             keyword: 'rhino',
             horns: 1
         }
-    ]);
 
+    ];
+
+    const filter = { keyword: 'rhino', horns: 2 };
+    //act
+    const filtered = makeImageFilter(images, filter);
+    //assert
+    assert.deepEqual(filtered, images);
 });
+
