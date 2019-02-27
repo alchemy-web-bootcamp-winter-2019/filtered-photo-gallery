fe0eb335aa2f893a93825204c8701e6a5dadc2d3
Cara Ponzini <ponzini@gmail.com>
2019-02-26 23:09:34 -0800

    working event listener logs filters in console

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import images from '../data/images.js';
 import makeImageTemplate from './make-image-template.js';
+//import filterImages from './filter-images.js';
 
 const imageDisplay = document.getElementById('image-display');
 
@@ -8,4 +9,23 @@ const imageDisplay = document.getElementById('image-display');
 images.forEach(image => {
     const dom = makeImageTemplate(image);
     imageDisplay.appendChild(dom);
+});
+
+const filterForm = document.getElementById('filter-form');
+
+filterForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(filterForm);
+    const hornsFilter = formData.get('horns');
+    let horns = 0;
+    if(hornsFilter) {
+        horns = parseInt(hornsFilter);
+    }
+
+    const filter = {
+        keyword: formData.get('keyword'),
+        horns: horns
+    };
+    console.log(filter);
 });
