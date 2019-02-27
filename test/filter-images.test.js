b981436a0d6c5a05e0c9644bdb3d38ee77a4b7bd
Tommy Tran <tranttommy95@gmail.com>
2019-02-26 23:11:44 -0800

    Filter with Keyword test passed

--- test/filter-images.test.js
+++ test/filter-images.test.js
@@ -5,24 +5,24 @@ QUnit.module('Filter Testing');
 const images = [
     { title: 'first', keyword: 'yea', horns: 1 },
     { title: 'second', keyword: 'nah', horns: 2 },
-    { title: 'third', keyword: 'yea', horns: 3 },
+    { title: 'third', keyword: 'yea', horns: 3 }
 ];
 
 function filterImages(filter, images) {
-
+    return images.filter(image => image.keyword === filter.keyword);
 }
 
 test('Filter with Keyword', function(assert) {
     // Arrange
     const expected = [
         { title: 'first', keyword: 'yea', horns: 1 },
-        { title: 'second', keyword: 'yea', horns: 3 }
+        { title: 'third', keyword: 'yea', horns: 3 }
     ];
     const filter = { keyword: 'yea' };
 
     // Act
     const result = filterImages(filter, images);
-    
+
     // Assert
-    assert.equal(result, expected);
+    assert.deepEqual(result, expected);
 });
