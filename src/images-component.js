8db368b2ad05c9a79d07a10e371e9c213a9b58e9
Leslie Shea <leslieyshea@gmail.com>
2019-02-26 16:57:06 -0800

    created loadImages function and transferred the forEach from index.js to here

---
+++ src/images-component.js
@@ -0,0 +1,21 @@
+export default function loadImages(images) {
+    const imageListNode = document.getElementById('image-list');
+
+    images.forEach(image => {
+        const dom = makeImageTemplate(image);
+        imageListNode.appendChild(dom);
+    });
+}
+
+export function makeImageTemplate(image) {
+    const html = /*html*/
+    `<li>
+        <h2>${image.title}</h2>
+        <img src="${image.url}">
+    </li>
+    `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
