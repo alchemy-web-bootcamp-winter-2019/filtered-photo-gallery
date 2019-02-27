c59a42a18b435e482ab2436db05c5de1d4f061e2
heather <unphurlng@gmail.com>
2019-02-27 08:37:48 -0800

    moved function GREEN

--- test/image-component.test.js
+++ test/image-component.test.js
@@ -1,21 +1,8 @@
+import { createTemplate } from '../src/image-component.js';
 const test = QUnit.test;
 
 QUnit.module('CREATE IMAGE TEMPLATE');
 
-
-function createTemplate(image) {
-    const template = document.createElement('template');
-    const html = /*html*/`
-        <li>
-            <p>UniWhal</p>
-            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
-            <p>Horns: 1</p>
-        </li>
-    `;
-    template.innerHTML = html;
-    const dom = template.content;
-    return dom;
-}
 
 test('Create template', function(assert) {
     // arrange
