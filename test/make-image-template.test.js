56f87ad3e984b29b86e366482574b61755430cf9
Tommy Tran <tranttommy95@gmail.com>
2019-02-26 17:03:31 -0800

    dynamic interpolation test passed

--- test/make-image-template.test.js
+++ test/make-image-template.test.js
@@ -2,17 +2,17 @@ const test = QUnit.test;
 
 QUnit.module('Template Testing');
 
-function makeImageTemplate() {
-    return `
+function makeImageTemplate(image) {
+    return /*html*/`
         <li>
-            <h2>Basically a unicorn</h2>
-            <img src="https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg">
-            <h3>Horns: 1</h3>
+            <h2>${image.title}</h2>
+            <img src="${image.url}">
+            <h3>Horns: ${image.horns}</h3>
         </li>
     `;
 }
 
-test('Test dynamic template', function(assert) {
+test('Test dynamic template', assert => {
     // Arrange
     const expected = `
         <li>
@@ -21,8 +21,14 @@ test('Test dynamic template', function(assert) {
             <h3>Horns: 1</h3>
         </li>
     `;
+    
+    const image = {
+        title: 'Basically a unicorn',
+        url: 'https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg',
+        horns: 1
+    };
     // Act
-    const result = makeImageTemplate();
+    const result = makeImageTemplate(image);
 
     // Assert
     assert.equal(result, expected);
