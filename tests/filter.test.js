0030a0bada64224f4fb8e6013995c0a461d66512
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-27 08:29:41 -0800

    all three tests passing for filter function

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -2,6 +2,15 @@ const test = QUnit.test;
 
 QUnit.module('testing keyword and horns filters');
 
+function filterImages(filter, images) {
+    const results = images.filter(image => {
+        const keywordFilter = !filter.keyword || image.keyword === filter.keyword;
+        const hornFilter = !filter.horns || image.horns >= filter.horns;
+        return keywordFilter && hornFilter;
+    });
+    return results;
+}
+
 const images = [
     { keyword: 'narwhal', horns: 1 },
     { keyword: 'narwhal', horns: 2 },
@@ -16,6 +25,39 @@ test('testing to see if keyword filter works', assert => {
     };
     const expected = [
         { keyword: 'narwhal', horns: 1 },
+        { keyword: 'narwhal', horns: 2 }
+    ];
+
+    // act
+    const results = filterImages(filter, images);
+    // assert
+    assert.deepEqual(results, expected);
+});
+
+test('testing to see if horns filter works', assert => {
+    // arrange
+    const filter = {
+        keyword: '',
+        horns: '2'
+    };
+    const expected = [
+        { keyword: 'narwhal', horns: 2 },
+        { keyword: 'rhino', horns: 2 }
+    ];
+
+    // act
+    const results = filterImages(filter, images);
+    // assert
+    assert.deepEqual(results, expected);
+});
+
+test('testing to see if keyword and horn filters works', assert => {
+    // arrange
+    const filter = {
+        keyword: 'narwhal',
+        horns: '2'
+    };
+    const expected = [
         { keyword: 'narwhal', horns: 2 }
     ];
 
