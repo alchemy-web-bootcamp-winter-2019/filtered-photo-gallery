27cb993a9286811eafaab6688837bebd3c7656a2
Tommy Tran <tranttommy95@gmail.com>
2019-02-26 23:19:08 -0800

    Filter with number of Horns test passed

--- test/filter-images.test.js
+++ test/filter-images.test.js
@@ -10,7 +10,9 @@ const images = [
 
 function filterImages(filter, images) {
     return images.filter(image => {
-        return !filter.keyword || image.keyword === filter.keyword;
+        const hasKeyword = !filter.keyword || image.keyword === filter.keyword;
+        const hasHorns = !filter.horns || image.horns >= filter.horns;
+        return hasKeyword && hasHorns;
     });
 }
 
@@ -33,6 +35,21 @@ test('Filter with no Keyword', function(assert) {
     // Arrange
     const expected = images;
     const filter = { keyword: '' };
+
+    // Act
+    const result = filterImages(filter, images);
+
+    // Assert
+    assert.deepEqual(result, expected);
+});
+
+test('Filter with number of Horns', function(assert) {
+    // Arrange
+    const expected = [
+        { title: 'second', keyword: 'nah', horns: 2 },
+        { title: 'third', keyword: 'yea', horns: 3 }
+    ];
+    const filter = { horns: 2 };
 
     // Act
     const result = filterImages(filter, images);
