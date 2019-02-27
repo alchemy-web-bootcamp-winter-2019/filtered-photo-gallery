eefc1fb49f8dfee7a1e2df649e0020617fb728f1
Tommy Tran <tranttommy95@gmail.com>
2019-02-26 23:26:55 -0800

    filter-component works as non-imported in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import images from '../data/images.js';
 import loadImages from './image-component.js';
-import './filter-component.js';
+// import './filter-component.js';
 
 loadImages(images);
 
+const gefilterForm = document.getElementById('gefilter-form');
+
+gefilterForm.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    const formDaddy = new FormData(gefilterForm);
+    const filter = {
+        keyword: formDaddy.get('keyword-filter'),
+        horns: Number(formDaddy.get('horns-filter'))
+    };
+
+    const filteredImages = filterImages(filter, images);
+    loadImages(filteredImages);
+
+});
+
+function filterImages(filter, images) {
+    return images.filter(image => {
+        const hasKeyword = !filter.keyword || image.keyword === filter.keyword;
+        const hasHorns = !filter.horns || image.horns >= filter.horns;
+        return hasKeyword && hasHorns;
+    });
+}
