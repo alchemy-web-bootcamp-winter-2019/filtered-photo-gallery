9ef7ec43f6625d2ea859cdfa73a13eb977bb1a0e
Payton Lower <paytown@me.com>
2019-02-26 18:42:05 -0800

    moved makehtmltemplatetest into its own file. working on filters tests

--- test/tests.js
+++ test/tests.js
@@ -1,31 +1,11 @@
 const test = QUnit.test;
 
 import './html-equal.js';
-import makeHtmlTemplate from '../src/make-html.js';
+import './make-html.test.js';
 
 
-const image = {
-    url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
-    title: 'UniWhal',
-    horns: 1
-};
+// Testing for filters
 
+test('filter by keyword', assert => {
 
-
-
-
-// Testing for template functionality
-QUnit.module('template');
-test('testing html templates', assert => {
-    const result = makeHtmlTemplate(image);
-
-    const expected = `
-        <li>   
-            <h3>UniWhal</h3>
-            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"> 
-            <h4>Horns: 1</h4>
-        </li>
-        `;
-
-    assert.htmlEqual(result, expected);
 });
