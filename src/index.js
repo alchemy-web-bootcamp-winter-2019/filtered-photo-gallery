d474d5ea7649a21dbfdced7bc5ae3eea720d51ec
Tommy Tran <tranttommy95@gmail.com>
2019-02-26 23:34:31 -0800

    successful use of callback

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import images from '../data/images.js';
 import loadImages from './image-component.js';
-// import './filter-component.js';
+import loadFilter from './filter-component.js';
+import filterImages from './filter-images.js';
 
 loadImages(images);
 
-const gefilterForm = document.getElementById('gefilter-form');
-
-gefilterForm.addEventListener('submit', event => {
-    event.preventDefault();
-    
-    const formDaddy = new FormData(gefilterForm);
-    const filter = {
-        keyword: formDaddy.get('keyword-filter'),
-        horns: Number(formDaddy.get('horns-filter'))
-    };
-
-    const filteredImages = filterImages(filter, images);
-    loadImages(filteredImages);
-
+loadFilter(filter => {
+    loadImages(filterImages(filter, images));
 });
 
-function filterImages(filter, images) {
-    return images.filter(image => {
-        const hasKeyword = !filter.keyword || image.keyword === filter.keyword;
-        const hasHorns = !filter.horns || image.horns >= filter.horns;
-        return hasKeyword && hasHorns;
-    });
-}
+
