41b46b3e4235d7f4f5b1f20a307847d9625f2c30
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-26 20:51:00 -0800

    Second filterImages test passing

--- test/filter-images.test.js
+++ test/filter-images.test.js
@@ -4,18 +4,19 @@ QUnit.module('filtering by keyword and horns');
 
 function filterImages(filter, images) {
     return images.filter(image => {
-        const hasKeyword = image.keyword === filter.keyword;
+        const hasKeyword = !filter.keyword || image.keyword === filter.keyword;
         return hasKeyword;
     });
 }
 
+const images = [
+    { title: 'image one', keyword: 'blue', horns: 1 },
+    { title: 'image two', keyword: 'blue', horns: 2 },
+    { title: 'image three', keyword: 'red', horns: 2 }
+];
+
 test('filter by keyword, return only objects with that keyword', assert => {
     // arrange
-    const images = [
-        { title: 'image one', keyword: 'blue', horns: 1 },
-        { title: 'image two', keyword: 'blue', horns: 2 },
-        { title: 'image three', keyword: 'red', horns: 2 }
-    ];
     const filter = {
         keyword: 'blue'
     };
@@ -27,4 +28,16 @@ test('filter by keyword, return only objects with that keyword', assert => {
     const result = filterImages(filter, images);
     // assert
     assert.deepEqual(result, expected);
+});
+
+test('filter by keyword, if no keyword return all images', assert => {
+    // arrange
+    const filter = {
+        keyword: ''
+    };
+
+    // act
+    const result = filterImages(filter, images);
+    // assert
+    assert.deepEqual(result, images);
 });
