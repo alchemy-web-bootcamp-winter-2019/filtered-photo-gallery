96471bb04a0049b5177d1af29d589d3bdb0616a9
mcnadams <mcneilbo@oregonstate.edu>
2019-02-26 17:04:31 -0800

    moved loadFilter to own module

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import images from '../data/images.js';
 import { loadImages } from './make-html-template.js';
 import filterImages from './filter-images.js';
-
-const filterForm = document.getElementById('filter-form');
+import loadFilter from './load-filter.js';
 
 loadImages(images);
-
-function loadFilter(callback) {
-    filterForm.addEventListener('submit', event => {
-        event.preventDefault();
-
-        const formData = new FormData(filterForm);
-        const filter = {
-            keyword: formData.get('keyword-filter'),
-            horns: formData.get('horns-filter')
-        };
-        callback(filter);
-    });
-}
 
 loadFilter(filter => {
     const filteredImages = filterImages(filter, images);
