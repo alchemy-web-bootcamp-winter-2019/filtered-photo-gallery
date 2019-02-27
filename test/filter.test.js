012eb4fa9e620b276f68d0580a6d6e60b8b0f3b4
Ben Parisot <me@benparisot.com>
2019-02-26 21:32:47 -0800

    write and pass tests for filter by horns

--- test/filter.test.js
+++ test/filter.test.js
@@ -9,7 +9,8 @@ const imageArray = [
 function filterImages(imageArray, filter) {
     return imageArray.filter(image => {
         const hasKeyword = !filter.keyword || image.keyword === filter.keyword;
-        return hasKeyword;
+        const hasHorns = !filter.horns || image.horns === filter.horns;
+        return hasKeyword && hasHorns;
     });
 }
 
@@ -30,6 +31,37 @@ test('filter array by keyword', assert => {
 test('how all images if keyword is empty', assert => {
     const filter = {
         keyword: ''
+    };
+    const expected = [
+        { keyword: 'red', horns: 1 },
+        { keyword: 'red', horns: 2 },
+        { keyword: 'blue', horns: 2 }
+    ];
+
+    const result = filterImages(imageArray, filter);
+
+    assert.deepEqual(result, expected);
+});
+
+test('filter by horns', assert => {
+    const filter = {
+        keyword: '',
+        horns: 2
+    };
+    const expected = [
+        { keyword: 'red', horns: 2 },
+        { keyword: 'blue', horns: 2 }
+    ];
+
+    const result = filterImages(imageArray, filter);
+
+    assert.deepEqual(result, expected);
+});
+
+test('how all images if horns is empty', assert => {
+    const filter = {
+        keyword: '',
+        horns: NaN
     };
     const expected = [
         { keyword: 'red', horns: 1 },
