ea727b5988fc64d0ee0c2511f2cda9cee5629285
Sean Nelson <smnelsonn@gmail.com>
2019-02-27 11:18:39 -0800

    added Travis 0.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import images from '../data/images.js';
-import { createHtmlTemplate } from './image.js';
 import filterImages from './filterImages.js';
 import loadImages from './image.js';
 import loadFilter from './loadFilter.js';
@@ -11,8 +10,6 @@ loadFilter(filterBy => {
     const filteredImages = filterImages(images, filterBy);
     loadImages(filteredImages);
 });
-
-
 
 
 
