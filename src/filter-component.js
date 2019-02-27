f3ab8d1baf44749f743e9c189a781664f21e8efd
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-26 22:42:54 -0800

    fixed loadFilter to include horns

--- src/filter-component.js
+++ src/filter-component.js
@@ -21,7 +21,8 @@ export default function loadFilter(callback) {
         }
 
         const filter = {
-            keyword: formData.get('keyword')
+            keyword: formData.get('keyword'),
+            horns: horns
         };
 
         callback(filter);
