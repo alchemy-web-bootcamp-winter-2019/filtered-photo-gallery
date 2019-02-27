64183d19e558adcf70e64520cd83b04f3c63ae9c
Leslie Shea <leslieyshea@gmail.com>
2019-02-26 16:37:32 -0800

    added template element and asked makeImageTemplate to return its content

--- test/make-image-list.test.js
+++ test/make-image-list.test.js
@@ -6,11 +6,13 @@ const test = QUnit.test;
 function makeImageTemplate(image) {
     const html = /*html*/
     `<li>
-        <h2>${image.title}<h2>
+        <h2>${image.title}</h2>
         <img src="${image.url}">
     </li>
     `;
-    return html;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
 }
 
 test('makes image list template', assert => {
@@ -21,11 +23,11 @@ test('makes image list template', assert => {
     const result = makeImageTemplate(image);
     const expected = /*html*/
     `<li>
-        <h2>UniWhal<h2>
+        <h2>UniWhal</h2>
         <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
     </li>
     `;
 
     // assert
-    assert.equal(result, expected);
+    assert.htmlEqual(result, expected);
 });
