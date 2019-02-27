d3f69a26c2a4e65a94f052daa23334c719cf23f9
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-26 16:53:24 -0800

    Dynamically creating the image list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-// eslint-disable-next-line no-console
-console.log('hello JavaScript!');
+import images from '../data/images.js';
+import { createImageTemplate } from './image-component.js';
+
+const imageList = document.getElementById('image-list');
+
+images.forEach(image => {
+    const dom = createImageTemplate(image);
+    imageList.appendChild(dom);
+});
