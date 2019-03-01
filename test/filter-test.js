f135c03508505f5c7afa7e8c92abb8c38c51c6e0
Peter Kotowski <bighoopee@gmail.com>
2019-02-28 18:08:00 -0800

    first test passed

--- test/filter-test.js
+++ test/filter-test.js
@@ -1,7 +1,53 @@
 const test = QUnit.test;
 
-QUnit.module('test suite two');
+QUnit.module('filter-test');
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+function filterImage(images, filter) {
+    const filterArray = images.filter(image => image.keyword === filter.keyword);
+    return filterArray;
+}
+
+
+
+
+test('write a test to sort image gallery by keyword', function(assert) {
+    //arrange
+    
+    const images = [
+        {
+            'keyword': 'narwhal',
+            'horns': 1
+        },
+        {
+            'keyword': 'rhino',
+            'horns': 2
+        },
+        {
+            'keyword': 'unicorn',
+            'horns': 1
+        }
+    ];
+    
+    const filter = 
+        
+    {
+        'keyword': 'narwhal',
+        
+    };
+    
+    //act
+    const result = filterImage(images, filter);
+    const expected = [
+        {
+            'keyword': 'narwhal',
+            'horns': 1
+        },
+        
+    ];
+
+
+
+    //assert
+
+    assert.deepEqual(result, expected);
 });
