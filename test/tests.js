ea713cade7451ef91e42e95d8755ecf9f3b9c01d
Mallarie <mallariemilano@gmail.com>
2019-03-01 13:53:10 -0800

    created gallery test and gallery build function

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,33 @@
 import './html-equal.js';
+const test = QUnit.test;
+
+function buildImageGallery(fakeAssImage) {
+    const galleryImage = `
+     <li>
+        <h2>${fakeAssImage.title}</h2>
+        <img src="${fakeAssImage.url}" alt="${fakeAssImage.description}">
+    </li>
+    `;
+    const template = document.createElement('template');
+    template.innerHTML = galleryImage;
+    return template.content;
+}
+
+test('to see if gallery will dynamically populate images', assert => {
+    //fake data
+    const fakeAssImage = {
+        title:'UniWhal',
+        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg', 
+        description: 'A unicorn and a narwhal nuzzling their horns'
+    };
+
+    //act
+    const result = buildImageGallery(fakeAssImage);
+    const expected = `
+    <li>
+        <h2>UniWhal</h2>
+        <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns">
+    </li>`;
+    //assert
+    assert.htmlEqual(result, expected);
+});
