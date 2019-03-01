98d753a93f388159d77b152f6b84d6b78ca0ee7d
Parker Helland <hellandp@gmail.com>
2019-02-28 22:52:05 -0800

    playing with css

---
+++ practice.js
@@ -0,0 +1,29 @@
+// ------- SORTING ------- \\
+
+
+// set up Table
+    // make headers <th>
+    // add data from 'contacts array of objects' into table 
+    // <tr>
+        //<td>$2,000</td>
+        //<td>John</td>
+    // </tr>
+
+// CSS data
+
+// Build Tests
+//  test('make contact row template')
+//      start with assert.htmlEqual(dom, `hardcoded html <tr></tr>`);
+//      act - const dom = makeContactRow(contact);
+//      arrange - const contact = {hardcoded <tr>}
+
+// Make function makeContactRow(contact) {
+//          const html = ` •hardcoded• 
+//              <tr><td> ${contact.title} </td></tr> `;
+//
+// ––––––––––––––– MOB 2 ––––––––––––––––
+//
+//
+//  makeContactRow template function (named export)
+//      
+//  loadContacts
