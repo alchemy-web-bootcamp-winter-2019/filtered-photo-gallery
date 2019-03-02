4a684a7975a61db8bb1ee60ebf6bf6b9540eca35
Mallarie <mallariemilano@gmail.com>
2019-03-01 17:01:36 -0800

    function to filter images by keyword and horn count

---
+++ src/filter-component.js
@@ -0,0 +1,23 @@
+const filterForm = document.getElementById('filter-input');
+
+function filterInput(callback) {
+    filterForm.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const formData = new FormData(filterForm);
+
+        let horns = 0;
+        const hornsInput = formData.get('horns');
+        if(hornsInput) {
+            horns = parseInt(hornsInput);
+        }
+
+        const filter = {
+            keyword: formData.get('keyword'),
+            horns: horns
+        };
+        callback(filter);
+    });
+}
+
+export default filterInput;
