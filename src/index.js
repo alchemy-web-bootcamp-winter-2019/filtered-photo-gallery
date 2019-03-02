dae02a7aaedb9ce1c4a8220dab408b5de362f74a
Mallarie <mallariemilano@gmail.com>
2019-03-01 16:52:42 -0800

    html for user input

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import fakeAssImages from '../data/images.js';
 import filterImages from './filter-images.js';
 import loadImages from './load-images.js'; 
-//import buildImageGallery from '../src/build-image-gallery.js';
 
 loadImages(fakeAssImages);
