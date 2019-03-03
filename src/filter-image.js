36f90e4c0a37fb25112eb275ddf3fc5e88e36e3c
Peter Kotowski <bighoopee@gmail.com>
2019-03-03 12:26:47 -0800

    fixed indentation probem

--- src/filter-image.js
+++ src/filter-image.js
@@ -1,4 +1,4 @@
- export default function filterImage(images, filter) {
+export default function filterImage(images, filter) {
     return images.filter(image => {
         const filterByKeyword = !filter.keyword || image.keyword === filter.keyword;
         const filterByHorns = !filter.horns || image.horns >= filter.horns;
