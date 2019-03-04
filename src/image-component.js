7be6f016515f0dc3a00756a6139eb544060b4a75
Cheri Portman <cheriportman@hotmail.com>
2019-03-03 16:35:44 -0800

    some styling

--- src/image-component.js
+++ src/image-component.js
@@ -4,7 +4,7 @@ export function makeImageComponent(image) {
     // console.log('image', image);
     const html = /*html*/`
     <li>
-    <h2>Name: ${image.title}</h2>
+    <h2 class="title">${image.title}</h2>
     <img src="${image.url}">
     <h2>Horns: ${image.horns}</h2>
     </li>
--- test/image-component.test.js
+++ test/image-component.test.js
@@ -21,7 +21,7 @@ test('create a template to create image tiles', function(assert) {
     //arrange
     const expected = /*html*/`
         <li>
-            <h2>Name: UniWhal</h2>
+            <h2 class="title">UniWhal</h2>
             <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
             <h2>Horns: 1</h2>
         </li>
