a28a6f1f92d130735c537f0ab30bd5479c9b4778
Parker Helland <hellandp@gmail.com>
2019-03-03 18:37:45 -0800

    cant figure it out. not grabbing horns value

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ loadImages(images);
 loadFilterFunction(theFilter => {
     const filtered = filterObjectsFunction(images, theFilter);
     loadImages(filtered);
-    clearImages();
-    const ulContainer = document.getElementById('ul-container');
-    function clearImages() {
-        while(ulContainer.children.length > 0) {
-            ulContainer.lastElementChild.remove();
-        } 
-    }
+    console.log(theFilter);
 });
 
