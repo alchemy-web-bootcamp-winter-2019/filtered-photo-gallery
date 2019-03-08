49c832cd1fcf34bf0d12a05d545dd2179fff5afe
heather <unphurlng@gmail.com>
2019-03-08 11:56:28 -0800

    uncommented test (determined not cause of travis fail)

--- test/image-component.test.js
+++ test/image-component.test.js
@@ -1,28 +1,28 @@
-// import { createTemplate } from '../src/image-component.js';
-// const test = QUnit.test;
+import { createTemplate } from '../src/image-component.js';
+const test = QUnit.test;
 
-// QUnit.module('CREATE IMAGE TEMPLATE');
+QUnit.module('CREATE IMAGE TEMPLATE');
 
 
-// test('Create template', function(assert) {
-//     // arrange
-//     const image = {
-//         title: 'UniWhal',
-//         url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
-//         horns: 1
-//     };
+test('Create template', function(assert) {
+    // arrange
+    const image = {
+        title: 'UniWhal',
+        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+        horns: 1
+    };
 
-//     const expected = /*html*/`
-//         <li>
-//             <p>UniWhal</p>
-//             <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
-//             <p>Horns: 1</p>
-//         </li>
-//     `;
+    const expected = /*html*/`
+        <li>
+            <p>UniWhal</p>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+            <p>Horns: 1</p>
+        </li>
+    `;
 
-//     // act
-//     const result = createTemplate(image);
+    // act
+    const result = createTemplate(image);
 
-//     // assert
-//     assert.htmlEqual(result, expected);
-// });
+    // assert
+    assert.htmlEqual(result, expected);
+});
