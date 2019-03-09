9868eda100ab2d54e79b102d613db0d42e514b98
heather <unphurlng@gmail.com>
2019-03-09 12:42:00 -0800

    more styling

--- src/image-component.js
+++ src/image-component.js
@@ -2,10 +2,10 @@ export function createTemplate(image) {
     const template = document.createElement('template');
     
     template.innerHTML = /*html*/`
-        <li>
-            <p>${image.title}</p>
+        <li class="image-card">
+            <p class="image-title">${image.title}</p>
             <img src="${image.url}">
-            <p>Horns: ${image.horns}</p>
+            <p class="horn-count">Horns: ${image.horns}</p>
         </li>
     `;
     return template.content;
