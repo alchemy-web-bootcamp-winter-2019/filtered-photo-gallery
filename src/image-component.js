295a86540b7481776418701d15335ed8c97977ae
heather <unphurlng@gmail.com>
2019-03-09 16:22:10 -0800

    deleted comments from components

--- src/image-component.js
+++ src/image-component.js
@@ -1,17 +1,3 @@
-/*
-- Use the Component Module pattern to have the `images-component.js` 
-    export a `loadImages` function used by `index.js`
-- Create the Filter Component. It will need to export `loadFilter` 
-    that accepts a callback. Remember the form work will have to 
-    happen inside the `loadFilter` function
-- Log the filter object in `index.js` so you know the Filter Component 
-    is wired up correctly.
-- TDD the `filterImages` function and move to own module
-- Incorporate the `filterImages` function into `index.js` and reload 
-    the images each time it changes.
-- Don't forget to remove the existing images when reloading!
-*/
-
 export function createTemplate(image) {
     const template = document.createElement('template');
     
