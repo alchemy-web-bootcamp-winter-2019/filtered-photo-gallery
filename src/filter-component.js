e63a4194fcc70dbf94cf6acd62e748edc1f550fc
heather <unphurlng@gmail.com>
2019-03-09 20:00:23 -0800

    final commit - filter function complete working as expected (after a lot of trial and error)

--- src/filter-component.js
+++ src/filter-component.js
@@ -1,9 +1,23 @@
-export function filteredImages(imagesArray, filtered) {
-    return imagesArray.filter((image) => {
+export function filterImages(imagesArray, filtered) {
+    return imagesArray.filter(image => {
         const hasKeyword = !filtered.keyword || image.keyword === filtered.keyword;
-        const hasHorns = !filtered.horns || image.horns >= filtered.horns;
+        const hasHorns = !filtered.horns || image.horns === filtered.horns;
         return hasKeyword && hasHorns;
     });
 }
 
-// Need callback function??
+export default function loadFilter(callback) {
+    const filterForm = document.getElementById('filter-form');
+
+    filterForm.addEventListener('submit', event => {
+        event.preventDefault();
+    
+        const formData = new FormData(filterForm);
+
+        const filterInput = {
+            keyword: formData.get('keyword-input'),
+            horns: parseInt(formData.get('horn-input'))
+        };
+        callback(filterInput);
+    });    
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import images from '../data/images.js';
 import { loadImages } from '../src/image-component.js';
+import { filterImages } from '../src/filter-component.js';
+import loadFilter from '../src/filter-component.js';
 
 loadImages(images);
 
-
-
-
-
-/*
-- Use the Component Module pattern to have the `images-component.js` 
-    export a `loadImages` function used by `index.js`
-- Create the Filter Component. It will need to export `loadFilter` 
-    that accepts a callback. Remember the form work will have to 
-    happen inside the `loadFilter` function
-- Log the filter object in `index.js` so you know the Filter Component 
-    is wired up correctly.
-- TDD the `filterImages` function and move to own module
-- Incorporate the `filterImages` function into `index.js` and reload 
-    the images each time it changes.
-- Don't forget to remove the existing images when reloading!
-*/
+loadFilter(filter => {
+    const filteredImages = filterImages(images, filter);
+    loadImages(filteredImages);
+});
