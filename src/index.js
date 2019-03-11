e888c590f2abba5637be28c2f878763da865f3da
Anna Ridenour <annamridenour@gmail.com>
2019-03-11 08:02:19 -0700

    created function getFilter

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+import images from '../assets/images.js';
+import { loadImages } from './loadImages.js';
+
+const filterForm = document.getElementById('filter-form');
+
+loadImages(images);
+
+function getFilter(callback) {
+    filterForm.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const formDaddy = new FormData(filterForm);
+
+        const filter = {
+            keyword: formDaddy.get('keyword-filter'),
+            horns: formDaddy.get('horns-filter')
+        };
+
+        callback(filter); 
+    });
+}
